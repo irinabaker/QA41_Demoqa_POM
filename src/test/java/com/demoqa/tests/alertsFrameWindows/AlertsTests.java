bc83c255ff303668cded351f4f96d6f2be65e729
irinabaker <paxpistor@gmail.com>
2024-07-04 18:21:11 +0200

    First commit

---
+++ src/test/java/com/demoqa/tests/alertsFrameWindows/AlertsTests.java
@@ -0,0 +1,34 @@
+package com.demoqa.tests.alertsFrameWindows;
+
+import com.demoqa.pages.AlertsFrameWindowsPage;
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.tests.TestBase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AlertsTests extends TestBase {
+
+    @BeforeMethod
+    public void precondition() {
+        new HomePage(driver).getAlertsFrameWindows();
+        new SidePanel(driver).selectAlerts();
+    }
+
+    @Test
+    public void waitAlertTest() {
+        new AlertsFrameWindowsPage(driver).waitAlert();
+    }
+
+    @Test
+    public void selectConfirmTest() {
+        new AlertsFrameWindowsPage(driver).selectResult("Cancel")
+                .verifyResult("Cancel");
+    }
+
+    @Test
+    public void sendMessageToAlertTest() {
+        new AlertsFrameWindowsPage(driver).sendMessageToAlert("Hello world!!!")
+                .verifyMessage("Hello world!!!");
+    }
+}
---
+++ src/test/java/com/demoqa/tests/elements/JavaScriptTest.java
@@ -0,0 +1,30 @@
+package com.demoqa.tests.elements;
+
+import com.demoqa.pages.ElementsPage;
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.tests.TestBase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class JavaScriptTest extends TestBase {
+
+    @BeforeMethod
+    public void precondition() {
+        new HomePage(driver).getElements();
+        new SidePanel(driver).selectTextBox();
+    }
+
+    @Test
+    public void JSExecutorTest() {
+        new ElementsPage(driver).enterDataWithJS("Jamal Musiala","jamal@gm.com")
+                .clickOnSubmitButtonWitJS()
+                .getInnerTextWithJS()
+                .refreshWithJS()
+                .getInnerTextWithJS()
+                .checkURLWithJs()
+                .navigateToNewPageWithJS("https://ticket-service-69443.firebaseapp.com")
+                .verifyTitleOfPageWithJS()
+                .generateAlertWithJS();
+    }
+}
